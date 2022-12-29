0c92da23c9cb65501e10de5ad860c1d36a1ba683
Soumya Sagnik Khanda <soumyasagnik@gmail.com>
2022-12-29 17:02:16 +0530

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -65,7 +65,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(id);
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://soumya-khanda-modern-ai.netlify.app/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
