15ac914d23feffdb633361eaf15006b6b2865b4b
Soumya Sagnik Khanda <soumyasagnik@gmail.com>
2022-12-29 16:53:32 +0530

    first commit

---
+++ dist/script.js
@@ -0,0 +1,96 @@
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector(".chat_container");
+const landingAnimation = document.querySelector(".landing-animation");
+
+let loadInterval;
+
+function loader(e) {
+  e.textContent = "";
+  loadInterval = setInterval(() => {
+    e.textContent += ".";
+    if (e.textContent === "....") e.textContent = "";
+  }, 300);
+}
+
+function typeText(e, ans) {
+  let index = 0;
+  let interval = setInterval(() => {
+    if (index < ans.length) {
+      e.innerHTML += ans.charAt(index);
+      index++;
+    } else clearInterval(interval);
+  }, 35);
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, id) {
+  return `
+    <div class="wrapper ${isAi && "ai"}">
+      <div class="chat">
+        <div class="profile">
+          <img
+            src="${isAi ? bot : user}"
+            alt="${isAi ? "bot" : "user"}"
+          />
+        </div>
+        <div class="message" id="${id}">${value}</div>
+      </div>
+    </div>
+    `;
+}
+
+const handleSubmit = async (e) => {
+  landingAnimation.remove();
+  e.preventDefault();
+  const data = new FormData(form);
+
+  // user
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+  form.reset();
+
+  // ai
+  const id = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", id);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  const messageDiv = document.getElementById(id);
+  loader(messageDiv);
+
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong...";
+    alert(err);
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) handleSubmit(e);
+});
---
+++ server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors()); // for cross origin requests and allow server to be called from UI
+app.use(express.json()); // for passing json from ui to server
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello World!",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0.0,
+      max_tokens: 3600,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
+
+app.listen(5000, () => console.log("Server is running"));
